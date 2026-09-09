1194c2b7766b6f5913bd340da5fabd1f680ff233
Julia Desmazes - ws <julia.desmazes@gmail.com>
2026-09-08 22:01:27 -0500

    add power ring around all the regions

--- librelane/split_pdn_cfg.tcl
+++ librelane/split_pdn_cfg.tcl
@@ -57,17 +57,19 @@ proc log_voltage_domains { name } {
 # define power domains
 #log_cmd set_voltage_domain -name $d_pwr -region $d_pwr -power VDDD -ground VSSD 
 log_cmd set_voltage_domain -name $d_pwr -power VDDD -ground VSSD 
-#log_cmd set_voltage_domain -name Core -region DIGITAL -power VDDD -ground VSSD 
+puts "Redefining $d_pwr with region DIGITAL"
+log_cmd set_voltage_domain -name $d_pwr -region DIGITAL -power VDDD -ground VSSD 
 log_cmd set_voltage_domain -name $a_pwr -region $a_pwr -power VDDA -ground VSSA
 log_cmd set_voltage_domain -name $a_pwr2 -region $a_pwr2 -power VDDA -ground VSSA
 
 global_connect -verbose
 
-log_db_core Core 
-log_db_core $d_pwr 
-log_db_core $a_pwr 
+#log_db_core Core 
+#log_db_core $d_pwr 
+#log_db_core $a_pwr 
+#log_voltage_domains Core
 
-log_voltage_domains Core
+pdn::report
 
 # debug
 #puts "volate domains: [get_voltage_domains]"
@@ -75,49 +77,62 @@ log_voltage_domains Core
 # don't add a core ring as we don't have unified power
 
 # digital power grid: don't have an analog power grid, analog will handle it 
-
-puts "\[INFO\] Defining digital voltage domain $d_pwr" 
 puts "\[INFO\] PDN Multilayer $::env(PDN_MULTILAYER)" 
 puts "\[INFO\] PDN vertical layer $::env(PDN_VERTICAL_LAYER)" 
 puts "\[INFO\] PDN horizontal layer $::env(PDN_HORIZONTAL_LAYER)" 
 
-set arg_list [list]
-if { $::env(PDN_ENABLE_PINS) } {
-	puts "\[INFO\] power layer vertical $::env(PDN_VERTICAL_LAYER) horizontal $::env(PDN_HORIZONTAL_LAYER)" 
-    lappend arg_list -pins "$::env(PDN_VERTICAL_LAYER) $::env(PDN_HORIZONTAL_LAYER)"
+
+proc add_pwr_grid { domain grid_name } {
+	puts "\[INFO\] Defining voltage domain $domain" 
+	set arg_list [list]
+	if { $::env(PDN_ENABLE_PINS) } {
+		puts "\[INFO\] power layer vertical $::env(PDN_VERTICAL_LAYER) horizontal $::env(PDN_HORIZONTAL_LAYER)" 
+	    lappend arg_list -pins "$::env(PDN_VERTICAL_LAYER) $::env(PDN_HORIZONTAL_LAYER)"
+	}
+	
+	log_cmd define_pdn_grid \
+	    -name $grid_name \
+	    -starts_with POWER \
+	    -voltage_domain $domain \
+	    {*}$arg_list
+	
+	set arg_list [list]
+	append_if_equals arg_list PDN_EXTEND_TO "core_ring" -extend_to_core_ring
+	append_if_equals arg_list PDN_EXTEND_TO "boundary" -extend_to_boundary
+	
+	log_cmd add_pdn_stripe \
+	    -grid $grid_name \
+	    -layer $::env(PDN_VERTICAL_LAYER) \
+	    -width $::env(PDN_VWIDTH) \
+	    -pitch $::env(PDN_VPITCH) \
+	    -offset $::env(PDN_VOFFSET) \
+	    -spacing $::env(PDN_VSPACING) \
+	    -starts_with POWER \
+	    {*}$arg_list
+	
+	log_cmd add_pdn_stripe \
+	    -grid $grid_name \
+	    -layer $::env(PDN_HORIZONTAL_LAYER) \
+	    -width $::env(PDN_HWIDTH) \
+	    -pitch $::env(PDN_HPITCH) \
+	    -offset $::env(PDN_HOFFSET) \
+	    -spacing $::env(PDN_HSPACING) \
+	    -starts_with POWER \
+	    {*}$arg_list
+	
+	log_cmd add_pdn_ring \
+		-grid $grid_name \
+		-layers "$::env(PDN_VERTICAL_LAYER) $::env(PDN_HORIZONTAL_LAYER)" \
+        -widths "$::env(PDN_CORE_RING_VWIDTH) $::env(PDN_CORE_RING_HWIDTH)" \
+        -spacings "$::env(PDN_CORE_RING_VSPACING) $::env(PDN_CORE_RING_HSPACING)" \
+        -core_offset "$::env(PDN_CORE_RING_VOFFSET) $::env(PDN_CORE_RING_HOFFSET)" \
+		-connect_to_pads
+
+	log_cmd add_pdn_connect \
+	    -grid $grid_name \
+	    -layers "$::env(PDN_VERTICAL_LAYER) $::env(PDN_HORIZONTAL_LAYER)"
 }
 
-log_cmd define_pdn_grid \
-    -name stdcell_grid \
-    -starts_with POWER \
-    -voltage_domain $d_pwr \
-    {*}$arg_list
-
-set arg_list [list]
-append_if_equals arg_list PDN_EXTEND_TO "core_ring" -extend_to_core_ring
-append_if_equals arg_list PDN_EXTEND_TO "boundary" -extend_to_boundary
-
-log_cmd add_pdn_stripe \
-    -grid stdcell_grid \
-    -layer $::env(PDN_VERTICAL_LAYER) \
-    -width $::env(PDN_VWIDTH) \
-    -pitch $::env(PDN_VPITCH) \
-    -offset $::env(PDN_VOFFSET) \
-    -spacing $::env(PDN_VSPACING) \
-    -starts_with POWER \
-    {*}$arg_list
-
-log_cmd add_pdn_stripe \
-    -grid stdcell_grid \
-    -layer $::env(PDN_HORIZONTAL_LAYER) \
-    -width $::env(PDN_HWIDTH) \
-    -pitch $::env(PDN_HPITCH) \
-    -offset $::env(PDN_HOFFSET) \
-    -spacing $::env(PDN_HSPACING) \
-    -starts_with POWER \
-    {*}$arg_list
-
-log_cmd add_pdn_connect \
-    -grid stdcell_grid \
-    -layers "$::env(PDN_VERTICAL_LAYER) $::env(PDN_HORIZONTAL_LAYER)"
-
+add_pwr_grid $d_pwr digital_std_gates	
+add_pwr_grid $a_pwr tmp_analog_grid
+add_pwr_grid $a_pwr2 tmp_analog_grid2	
